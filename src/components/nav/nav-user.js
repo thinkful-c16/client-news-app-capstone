e5db61caab4935fef32ab96f7be95c895c9a456c
Owcollier <owcollier@gmail.com>
2018-02-20 15:39:15 -0500

    basic nav-user component built out

---
+++ src/components/nav/nav-user.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+export class NavUser extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="nav-username">
+         Hello {this.props.userName}!
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  userName: state.auth.currentUser.username
+});
