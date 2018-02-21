dadf3835ab0bb8a8bfcfab3ea1b0e222df98ce1d
Owcollier <owcollier@gmail.com>
2018-02-21 16:46:35 -0500

    added logout button to nav-user component

--- src/components/nav/nav-user.js
+++ src/components/nav/nav-user.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {clearAuth} from '../../actions/auth';
+import {clearAuthToken} from '../../local-storage';
 
 export class NavUser extends React.Component {
 
+logOut() {
+  this.props.dispatch(clearAuth());
+  clearAuthToken();
+}
+
   render() {
+
+    let logOutButton;
+    if (this.props.loggedIn) {
+        logOutButton = (
+            <button onClick={() => this.logOut()}>Log out</button>
+        );
+    }
+
     return (
       <div className="nav-username">
-         Hello {this.props.userName}!
+        <span>Hello {this.props.userName}!</span>
+        {logOutButton}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  userName: state.auth.currentUser.username
+  userName: 'first and last goes here',
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(NavUser);
