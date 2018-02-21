00aa2dbba2cd29916103b00ddebfd91ba16cd534
Owcollier <owcollier@gmail.com>
2018-02-21 14:13:32 -0500

    preliminary explore component built out with hardcoded data, dashboard wired up to app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LoginForm from './auth/login-form.js';
 import RegistrationForm from './auth/registration-form.js';
+import Dashboard from './dashboard/dashboard.js';
 import Nav from './nav/nav.js';
 import { Route, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -17,6 +18,7 @@ export class App extends Component {
         <Nav />
         <Route exact path="/" component={LoginForm} />
         <Route exact path="/register" component={RegistrationForm} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </div>
     );
   }
