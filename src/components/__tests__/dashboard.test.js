75096eb9ba00f3083023030d8120cfc3f680eadd
Nicci Williams <nicci@nicciwill.com>
2018-02-26 11:37:10 -0500

    Working on tests

--- src/components/__tests__/dashboard.test.js
+++ src/components/__tests__/dashboard.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 
 import { Dashboard } from '../dashboard/dashboard';
+import { fetchTopHeadlines } from '../../actions/api';
 
 describe('<Dashboard />', () => {
-  it('Renders without crashing', () => {
-    shallow(<Dashboard />);
+  it('Dispatches fetchTopHeadlines on componentDidMount', () => {
+    const dispatch = jest.fn();
+    const index = 2;
+    const wrapper = shallow(
+      <Dashboard dispatch={dispatch}/>
+    );
+    const instance = wrapper.instance();
+    instance.fetchTopHeadlines();
+    expect(dispatch).toHaveBeenCalled(fetchTopHeadlines());
   })
 })
 
