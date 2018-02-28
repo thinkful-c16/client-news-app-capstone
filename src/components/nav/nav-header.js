2f9d64971d3a0eea3b43e24f19d961ebbc577939
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-28 13:20:00 -0800

    added logo, fixed styling

--- src/components/nav/nav-header.js
+++ src/components/nav/nav-header.js
@@ -5,7 +5,8 @@ export default function NavHeader(props) {
   return (
     <div className="nav-header-div">
       <header className="nav-header">
-          <h1 className="nav-title">NEWS SPOT</h1>
+        <img src="../../images/logo.png" alt="logo"/>
+        <h1 className="nav-title">NEWS SPOT</h1>
       </header>
     </div>
   );
