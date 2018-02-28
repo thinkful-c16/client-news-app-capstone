34fd0f1451958fe937f725d764b7ddd40392580c
Owcollier <owcollier@gmail.com>
2018-02-28 10:57:52 -0500

    added error message to collection creation modal if user submits form without filling the collection name input

--- src/components/collections/collections-create-modal.js
+++ src/components/collections/collections-create-modal.js
@@ -10,7 +10,9 @@ export class CollectionsCreateModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      collectionNameValue: ''
+      collectionNameValue: '',
+      errorMessage: null
+      
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,13 +20,21 @@ export class CollectionsCreateModal extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({collectionNameValue: event.target.value});
+    this.setState({
+      collectionNameValue: event.target.value,
+      errorMessage: null
+    });
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.dispatch(actions.createCollection(this.state.collectionNameValue));
-    this.props.onCloseModal();
+    const input = this.state.collectionNameValue.trim();
+    if (input !== '') {
+      this.props.dispatch(actions.createCollection(this.state.collectionNameValue));
+      this.props.onCloseModal();
+    } else {
+      this.setState({errorMessage: 'Collection name cannot be empty! Please add a name for your new collection.'})
+    }
   }
 
   render() {
@@ -39,6 +49,7 @@ export class CollectionsCreateModal extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label className="collection-name-label" htmlFor="collectionNameInput">Name</label>
           <input type="text" value={this.state.collectionNameValue} onChange={this.handleChange} className="collection-name-input" id="collectionNameInput"/>
+          <div className="error-message-div">{this.state.errorMessage}</div>
           <input type="submit" className="create-collection-submit" value="Create"/>
         </form>
       </div>
