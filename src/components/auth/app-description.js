fb3eb930f39488e0d7b33b5ffd702b02f65b14e4
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-03-07 09:43:51 -0800

    updated the about section on the login page

--- src/components/auth/app-description.js
+++ src/components/auth/app-description.js
@@ -1,20 +1,81 @@
 import React from 'react';
 import '../../styles/app-description.css';
+import FontAwesome from 'react-fontawesome';
 
 export default class AppDescription extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      discoverClass: 'hidden',
+      curateClass: 'hidden',
+      shareClass: 'hidden'
+    }
+    this.toggleDiscover = this.toggleDiscover.bind(this);
+    this.toggleCurate = this.toggleCurate.bind(this);
+    this.toggleShare = this.toggleShare.bind(this);
+  }
+  
+  toggleDiscover(){
+    if (this.state.discoverClass === "hidden") {
+      this.setState({discoverClass: "show"})
+    }
+    else{
+      this.setState({discoverClass: "hidden"})
+    }
+  }
+
+  toggleCurate(){
+    if (this.state.curateClass === "hidden") {
+      this.setState({curateClass: "show"})
+    }
+    else{
+      this.setState({curateClass: "hidden"})
+    }
+  }
+
+  toggleShare(){
+    if (this.state.shareClass === "hidden") {
+      this.setState({shareClass: "show"})
+    }
+    else{
+      this.setState({shareClass: "hidden"})
+    }
+  }
+
   render() {
+
     return(
       <div className="app-desc-container">
-        <h2 className="share-head">
-          Share news in a meaningful way
-        </h2>
         <div className="desc-thumb-div">
-          <div className="desc-thumbnails"></div>
-          <div className="desc-thumbnails"></div>
-          <div className="desc-thumbnails"></div>
+          <div className="desc-thumbnails">
+            <div className="discover" onMouseOver={this.toggleDiscover} onMouseOut={this.toggleDiscover}>
+              <span><FontAwesome name='lightbulb' /></span>
+              <span>DISCOVER</span>
+            </div>
+          </div>
+          <div className="desc-thumbnails">
+            <div className="curate" onMouseOver={this.toggleCurate} onMouseOut={this.toggleCurate}>
+              <span><FontAwesome name='book'/></span>
+              <span>CURATE</span>
+            </div>
+          </div>
+          <div className="desc-thumbnails">
+            <div className="share" onMouseOver={this.toggleShare} onMouseOut={this.toggleShare}>
+              <span><FontAwesome name='share-square'/></span>
+              <span>SHARE</span>
+            </div>
+          </div>
         </div>
-        <div className="desc-paragraph-div">
-          <p className="app-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit lorem non turpis sodales, sit amet pretium sem aliquet. Quisque et purus vel ligula gravida tincidunt. Quisque vel semper nunc. Aenean ullamcorper ultrices est at varius. Vivamus sit amet ligula congue risus malesuada volutpat sit amet id ex.</p>
+        <div className="desc-text">
+          <div className={this.state.discoverClass} onMouseOver={this.toggleDiscover}>
+            Find news articles by keyword or category from all of the major, reputable media outlets.
+          </div>
+          <div className={this.state.curateClass} onMouseOver={this.toggleCurate}>
+            Create your own collections of news articles to save for later or share.
+          </div>
+          <div className={this.state.shareClass} onMouseOver={this.toggleShare}>
+            Share articles to social media, or see what others are sharing.
+          </div>
         </div>
       </div>
     )
